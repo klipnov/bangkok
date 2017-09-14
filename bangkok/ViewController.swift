86e4655cc28b92d6b0ea4920ae0362ca85a05e15
ShahQays Bin Md. Shah <shahqays@innovez-one.com>
2017-09-14 19:34:34 +0800

    Initial Commit

---
+++ bangkok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bangkok
+//
+//  Created by Shah Qays on 14/09/2017.
+//  Copyright © 2017 Shah Qays. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
