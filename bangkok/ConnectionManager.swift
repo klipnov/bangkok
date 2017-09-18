bdfaabb88abfb8b7a052ca2778289af64d62ca44
ShahQays Bin Md. Shah <shahqays@innovez-one.com>
2017-09-18 19:26:08 +0800

    Added ConnectionManager and Endpoints

---
+++ bangkok/ConnectionManager.swift
@@ -0,0 +1,14 @@
+//
+//  ConnectionManager.swift
+//  bangkok
+//
+//  Created by Shah Qays on 18/09/2017.
+//  Copyright © 2017 Shah Qays. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ConnectionManager {
+    
+}
---
+++ bangkok/Endpoints.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoints.swift
+//  bangkok
+//
+//  Created by Shah Qays on 18/09/2017.
+//  Copyright © 2017 Shah Qays. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoints: String {
+    case surveys = "https://nimbl3-survey-api.herokuapp.com/surveys.json"
+}
